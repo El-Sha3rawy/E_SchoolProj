e92804006e8583250b3569b239c68a177ec714c8
El-Sha3rawy <81260400+El-Sha3rawy@users.noreply.github.com>
2026-07-04 11:39:17 +0300

    Add files via upload

---
+++ E_SchoolProj/Proj/Program.cs
@@ -0,0 +1,44 @@
+using Application;
+using Infrastructure;
+using Persistence;
+using Persistence.Initialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddPersistence(builder.Configuration);
+
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var initializer =
+        scope.ServiceProvider
+            .GetRequiredService<AppDbInitializer>();
+
+    await initializer.InitializeAsync();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
